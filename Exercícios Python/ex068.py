bef036390faa475b7c1fed487f3db4901af8add8
ArthurCampigotto <campigotto65@gmail.com>
2022-07-07 09:35:17 -0300

    ex068

---
+++ Exercícios Python/ex068.py
@@ -0,0 +1,26 @@
+from random import randint
+v = 0
+while True:
+    jogador = int(input('Diga um valor: '))
+    computador = randint(0,10)
+    total = jogador + computador
+    tipo = ' '    
+    while tipo not in 'PI':
+        tipo = str(input('Par ou impar? [P/I] ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador jogou {computador}. Total de {total} ', end= '')
+    print('DEU PAR' if total % 2 == 0 else 'DEU ÍMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu !')
+            v += 1
+        else:
+            print('Você perdeu !')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você venceu !')
+        else:
+            print('Você perdeu !')
+            break
+    print('Vamos jogar novamente...')
+print(f'Game Over ! Você venceu {v} vezes')
