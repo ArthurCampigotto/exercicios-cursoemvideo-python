d8f4337ff0b65e0ad43190a66689c4416fe3786f
ArthurCampigotto <campigotto65@gmail.com>
2022-08-01 20:58:49 -0300

    ex079

--- ex079.py
+++ ex079.py
@@ -1,11 +1,14 @@
-listanum = []
+numeros = list()
 while True:
-    listanum.append(int(input('Digite um número: ')))
-    
-    r = str(input('Quer continuar ? [S/N]'))
+    n = int(input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso....')
+    else:
+        print('Valor duplicado! Não ira ser adicionado')
+    r = str(input('Quer continuar? [S/N]'))
     if r in 'Nn':
         break
-
-    print('Valor adicionado com sucesso !')
-    print('Quer continuar ? [S/N]')
-    
+print('-='*30)
+numeros.sort()
+print(f'Você digitou os valores {numeros}')
